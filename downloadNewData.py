6217a7039a52cb7b9150f791e757794bfb8c7b28
Andy Wu <andywu0913@yahoo.com.tw>
2019-09-11 15:56:20 +0800

    Modify downloadNewData.py to check the varification code url before requesting it.

--- downloadNewData.py
+++ downloadNewData.py
@@ -26,8 +26,11 @@ for i in range(times):
     page_content = opener.open('https://www.ais.tku.edu.tw/EleCos/login.aspx').read().decode('utf-8')
 
     # get the varification code image
-    varification_code_rul = 'https://www.ais.tku.edu.tw/EleCos/BaseData/{0}'.format(re.findall('confirm.ashx.[^"]*', page_content)[0])
-    img_binary = opener.open(varification_code_rul).read()
+    varification_code_url = 'https://www.ais.tku.edu.tw/EleCos/BaseData/{0}'.format(re.findall('confirm.ashx.[^"]*', page_content))
+    if len(varification_code_url) == 0:
+        print('Cannot locate the verification code image.')
+        exit(-1)
+    img_binary = opener.open(varification_code_url[0]).read()
 
     varification_code = ''
 
