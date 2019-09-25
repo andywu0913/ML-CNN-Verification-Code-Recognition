0bd2bb67e51a6375c09aa849f2cda4556925d39a
AndyWu <andywu0913@yahoo.com.tw>
2019-09-25 05:59:19 +0000

    Add ppt file for demo.

--- train.py
+++ train.py
@@ -1,7 +1,6 @@
 import numpy as np
 import os
 import random
-from PIL import Image
 from sklearn.model_selection import train_test_split
 from tensorflow import keras
 from tensorflow.keras import layers
@@ -38,7 +37,7 @@ for img_filename in img_filenames:
     split_digits_in_img(img_array, x_list, y_list)
 y_list = keras.utils.to_categorical(y_list, num_classes=10)
 
-# split data into training set and test set
+# split data into training set and testing set
 x_train, x_test, y_train, y_test = train_test_split(x_list, y_list)
 
 # model
@@ -47,7 +46,7 @@ if os.path.isfile('model/cnn_model.h5'):
     model = models.load_model('model/cnn_model.h5')
     print('Model loaded from file.')
 else:
-    # create a new cnn model
+    # otherwise, create a new cnn model
     model = models.Sequential()
     model.add(layers.Conv2D(32, kernel_size=(3, 3), activation='relu', input_shape=(img_rows, img_cols // digits_in_img, 1)))
     model.add(layers.Conv2D(64, (3, 3), activation='relu'))
@@ -57,7 +56,7 @@ else:
     model.add(layers.Dense(128, activation='relu'))
     model.add(layers.Dropout(rate=0.5))
     model.add(layers.Dense(10, activation='softmax'))
-    print('New created model.')
+    print('New model created.')
 
 model.compile(loss=keras.losses.categorical_crossentropy, optimizer=keras.optimizers.Adadelta(), metrics=['accuracy'])
 
